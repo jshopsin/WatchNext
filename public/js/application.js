d7cad4fbd5ded5f557aa8c90a169a14ad5be518d
Jennifer Shopsin <jshopsin@yahoo.com>
2017-08-25 15:26:07 -0700

    add media hound api

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  mediaHoundAuthentication();
+  mediaHoundMovieSearch();
 });
+
+var mediaHoundAuthentication = function() {
+  import { MHSDK } from 'houndjs';
+
+  MHSDK.configure('mhclt_WatchNext', 'PMlt2Py3u2RjQ4RStewdGauEYkO1rki1u1KON2hw1te7uaIi');
+};
+
+var mediaHoundMovieSearch = function(movieTitle) {
+  MHSearch.fetchResultsForSearchTerm(movieTitle, [MHSearch.SCOPE_MOVIE])
+  .then(response => {
+    const movie = response.content[0].object;
+    console.log('First result:', movie.metadata.name);
+  });
+};
